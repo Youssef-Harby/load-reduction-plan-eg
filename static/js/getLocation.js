f1fd9c527918c1eeba26ee5191bb55825e34a148
Youssef-Harby <youssef_harby@yahoo.com>
2023-08-04 02:30:18 +0300

    refactor and error handle get location

--- static/js/getLocation.js
+++ static/js/getLocation.js
@@ -1,7 +1,7 @@
 document.getElementById('getLocation').addEventListener('click', function () {
     // Check if Geolocation is supported
     if (!navigator.geolocation) {
-        console.log("Geolocation is not supported by this browser.");
+        alert("Geolocation is not supported by your browser.");
         return;
     }
 
@@ -10,21 +10,34 @@ document.getElementById('getLocation').addEventListener('click', function () {
         // Extract the coordinates
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
-        console.log("Latitude: " + latitude + ", Longitude: " + longitude);
 
         // Remove previous location marker if it exists
         if (window.locationMarker) {
             window.locationMarker.remove();
         }
 
-        // Create a blue dot to represent the location
+        // Create a blue dot to represent the location using maplibregl
         window.locationMarker = new maplibregl.Marker({
             color: 'blue',
             draggable: false
         })
             .setLngLat([longitude, latitude])
-            .addTo(map);
+            .addTo(map); // Assuming 'map' is your maplibregl map instance
     }, function (error) {
-        console.log("An error occurred while retrieving location: " + error.message);
+        // Handle errors and provide user feedback
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                alert("Permission to access location was denied.");
+                break;
+            case error.POSITION_UNAVAILABLE:
+                alert("Location information is unavailable.");
+                break;
+            case error.TIMEOUT:
+                alert("The request to get user location timed out.");
+                break;
+            case error.UNKNOWN_ERROR:
+                alert("An unknown error occurred while retrieving location.");
+                break;
+        }
     });
 });
