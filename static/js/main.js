58b2679d11b1c1e4b6535aef544bb919fcab6ff8
Youssef-Harby <youssef_harby@yahoo.com>
2023-08-04 00:51:12 +0300

    change the api key of maptiler

--- static/js/main.js
+++ static/js/main.js
@@ -6,7 +6,7 @@ maplibregl.setRTLTextPlugin(
 
 const map = new maplibregl.Map({
     container: "map",
-    style: "https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL",
+    style: "https://api.maptiler.com/maps/streets/style.json?key=KDhMfHvorAFkFe64wlZb",
     center: [31.2357, 30.0444],
     zoom: 8.5,
 });
